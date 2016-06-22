d30f8013ea65be909741d8bc917967dbf7bf898b
CoderXC <xiaobing_mailbox@163.com>
2016-06-22 14:21:18 +0800

    Initial Commit

---
+++ CircularLoadingProgressView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircularLoadingProgressView
+//
+//  Created by 小蔡 on 16/6/22.
+//  Copyright © 2016年 小蔡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircularLoadingProgressView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircularLoadingProgressView
+//
+//  Created by 小蔡 on 16/6/22.
+//  Copyright © 2016年 小蔡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
